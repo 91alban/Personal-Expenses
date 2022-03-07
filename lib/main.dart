d5a2e52407ef5a984504dff525212e4b32c23d6b
alban_loku <alban@entalgo.com>
2022-03-07 16:55:11 +0100

    changes on main

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,11 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
-  String? titleInput;
-  String? amountInput;
+  // String? titleInput;
+  // String? amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -59,15 +62,17 @@ class MyHomePage extends StatelessWidget {
                 children: [
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
-                    onChanged: (value) {
-                      titleInput = value;
-                    },
+                    controller: titleController,
+                    // onChanged: (value) {
+                    //   titleInput = value;
+                    // },
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
-                    onChanged: (value) {
-                      amountInput = value;
-                    },
+                    controller: amountController,
+                    // onChanged: (value) {
+                    //   amountInput = value;
+                    // },
                   ),
                   FlatButton(
                     child: Text('Add Transaction'),
